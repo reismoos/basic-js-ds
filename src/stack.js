a3478f35e1b297f24846d5caca7340f58a5ab011
Dmitry <Reismoos@gmail.om>
2022-10-17 13:10:09 +0300

    finish stack

---
+++ src/stack.js
@@ -0,0 +1,36 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+class Stack {
+
+  constructor(){
+    this.items = []
+  }
+
+  push(el) {
+    this.items.push(el)
+  }
+
+  pop() {
+    return this.items.pop()
+  }
+
+  peek() {
+    return this.items[this.items.length-1]
+  }
+}
+
+module.exports = {
+  Stack
+};
